f34d48a54290a8c3f8d12ecca62ee18415f30cec
Steve Viens <steve@viens.net>
2021-12-19 23:03:09 -0500

    removing unneccessary sagemaker cft work

--- cli/create_sagemaker_stack.sh
+++
@@ -1,8 +0,0 @@
-aws cloudformation create-stack \
-            --stack-name 'hw3-sagemaker-stack' \
-            --capabilities CAPABILITY_NAMED_IAM \
-            --template-body file://../cft/sagemaker_stack.yml
-
-# REFERENCES:
-#   AWS CloudFormation CLI Command Reference v2
-#   https://awscli.amazonaws.com/v2/documentation/api/latest/reference/cloudformation/index.html
--- cli/delete_sagemaker_stack.sh
+++
@@ -1,6 +0,0 @@
-aws cloudformation delete-stack \
-            --stack-name 'hw3-sagemaker-stack'
-
-# REFERENCES:
-#   AWS CloudFormation CLI Command Reference v2
-#   https://awscli.amazonaws.com/v2/documentation/api/latest/reference/cloudformation/index.html
